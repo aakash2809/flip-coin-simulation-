0bf1ec28d71e918efb362eda3c72fa0a136b72df
aakash2809 <aakashrajak289@gmail.com>
2020-10-31 03:44:01 +0530

    Add : random win heads or tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -x
 
-echo "                           Welcome to fliping coin simutor"
+echo "                           Welcome to fliping coin simulator"
 echo "----------------------------------------------------------------------------------"
+
+result=$((RANDOM%2));
+if [ $result -eq 0 ]
+   then
+         echo "Heads"
+   else
+         echo "Tails"
+fi
