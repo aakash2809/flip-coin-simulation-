38ca1e08e76705af8020dbb3bd39a4be117604d9
aakash2809 <aakashrajak289@gmail.com>
2020-10-30 18:34:12 +0530

    initial commit

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -x
 
-echo "Welcome to fliping coin simutor"
+echo "                           Welcome to fliping coin simutor"
+echo "----------------------------------------------------------------------------------"
