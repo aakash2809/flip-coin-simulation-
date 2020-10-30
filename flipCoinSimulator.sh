ba023b7f92c3d0d7f220342af33e3a4c7aa32e10
aakash2809 <aakashrajak289@gmail.com>
2020-10-31 04:51:23 +0530

    tie and win

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -6,8 +6,8 @@ count=0
 echo "                           Welcome to fliping coin simulator"
 echo "----------------------------------------------------------------------------------"
 
-#GET NUMBER OF TIMES HEAD AND TAIL WON
-while [ $count -lt 20 ]
+#FLIP COIN WIN COUNT BECOME 21 EITHER FOR HEAD OR FOR TAIL 
+while [[ $headCount -lt 21 && $tailCount -lt 21 ]]
 do
     result=$(( RANDOM%2 ))
     if [ $result -eq 1 ]
@@ -15,10 +15,26 @@ do
           ((headCount++))
     else
           ((tailCount++))
-    fi   
+    fi
 
     ((count++))
 done
 
 echo "Heads won " $headCount " times "
 echo "Tails won " $tailCount " times "
+
+#TIE CONDITION
+if [ $headCount -eq $tailCount ]
+then
+		echo "tie"
+elif [ $headCount -gt $tailCount  ]
+then
+		win=$(($headCount-$tailCount))
+		echo "Heads win by " $win
+else
+		 win=$(($tailCount-$headCount))
+      echo "Tails win by "$win
+
+
+fi
+
