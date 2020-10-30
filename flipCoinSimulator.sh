17cdbb9f2321131d737eec7d5d5fc95b1ee53580
aakash2809 <aakashrajak289@gmail.com>
2020-10-31 04:10:41 +0530

    filp and compute number of win for heads or tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,12 +1,24 @@
-#!/bin/bash -x
+#!/bin/bash 
+headCount=0
+tailCount=0
+count=0
 
 echo "                           Welcome to fliping coin simulator"
 echo "----------------------------------------------------------------------------------"
 
-result=$((RANDOM%2));
-if [ $result -eq 0 ]
-   then
-         echo "Heads"
-   else
-         echo "Tails"
-fi
+#GET NUMBER OF TIMES HEAD AND TAIL WON
+while [ $count -lt 20 ]
+do
+    result=$(( RANDOM%2 ))
+    if [ $result -eq 1 ]
+    then
+          ((headCount++))
+    else
+          ((tailCount++))
+    fi   
+
+    ((count++))
+done
+
+echo "Heads won " $headCount " times "
+echo "Tails won " $tailCount " times "
